b197dbea711703fa4786c1e221aadfd1c7e21d03
talesxavier1 <Talesxavier1@gmail.com>
2024-04-09 22:31:05 -0300

    init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+var main = function () {
+    debugger;
+    var chartInstance;
+    chartInstance = $("#teste").dxButton({
+        text: "Frutas laminadas",
+        type: 'normal',
+        height: "100px",
+        onClick: function () {
+            var a = "";
+        }
+    }).dxButton("instance");
+};
+main();
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+
+const main = (): void => {
+    debugger;
+    var chartInstance: DevExpress.ui.dxButton;
+    chartInstance = $("#teste").dxButton({
+        text: "Frutas laminadas",
+        type: 'normal',
+        height: "100px",
+        onClick: () => {
+            let a = "";
+
+        }
+    }).dxButton("instance")
+}
+
+main();
