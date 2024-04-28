696a38ce7b9bc93eb486654bd0be8626304aba9d
talesxavier1 <Talesxavier1@gmail.com>
2024-04-27 22:07:23 -0300

    dev

--- src/dist/index.js
+++ src/dist/index.js
@@ -3,7 +3,7 @@ const main = () => {
     let diagram = new Diagram();
     diagram.setDiagramOptions(JSON.stringify(diagramProps), diagramData);
 };
-const diagramProps = { "page": { "width": 16782, "height": 23812, "pageColor": -1, "pageWidth": 8391, "pageHeight": 11906, "pageLandscape": false }, "connectors": [], "shapes": [{ "key": "45", "dataKey": "e7a3ee4b-0e0c-67ba-5d35-9793a7c1dac3", "locked": false, "zIndex": 0, "type": "multicast_in", "text": "", "x": 984, "y": 11082, "width": 3392, "height": 3593 }, { "key": "46", "dataKey": "4b68a366-5a78-0679-2dd3-be8b3ba16e24", "locked": false, "zIndex": 0, "type": "multicast_out", "text": "", "x": 8015, "y": 11611, "width": 3335, "height": 3164.0000000000005 }] };
-const diagramData = [{ "type": "multicast_in", "text": "", "ID": "e7a3ee4b-0e0c-67ba-5d35-9793a7c1dac3" }, { "type": "multicast_out", "text": "", "ID": "4b68a366-5a78-0679-2dd3-be8b3ba16e24" }];
+const diagramProps = { "page": { "width": 25173, "height": 11906, "pageColor": -1, "pageWidth": 8391, "pageHeight": 11906, "pageLandscape": false }, "connectors": [], "shapes": [{ "key": "48", "dataKey": "0817199e-713d-966e-aff7-514f0af6cd62", "locked": false, "zIndex": 0, "type": "processContainer", "text": "", "x": 4978, "y": 1988, "width": 14400, "height": 7200 }] };
+const diagramData = [{ "type": "processContainer", "text": "", "ID": "0817199e-713d-966e-aff7-514f0af6cd62" }];
 main();
 //# sourceMappingURL=index.js.map
