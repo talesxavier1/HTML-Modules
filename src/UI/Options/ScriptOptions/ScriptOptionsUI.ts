dc8bacf18d4ea746c705a8e974fcbd8bc6e37133
talesxavier1 <Talesxavier1@gmail.com>
2024-07-14 19:32:01 -0300

    dev

--- src/UI/Options/ScriptOptions/ScriptOptionsUI.ts
+++ src/UI/Options/ScriptOptions/ScriptOptionsUI.ts
@@ -4,21 +4,25 @@ import { ScriptModel } from "../../../models/ScriptModel";
 import { ComponentInstanceModel } from "../../../Utils/dx-utils/ComponentInstanceModel";
 import { InstanceProps } from "../../../Utils/dx-utils/InstanceProps";
 import { TESTE_AAA } from "../../../Utils/dx-utils/Consts";
+// declare var monaco: any;
 
 interface ITabScriptOptions {
     id: string,
     title: string,
     html: JQuery<HTMLElement>
 }
+
 export class ScriptOptionsUI implements IOptionUI {
     private componentInstanceModel = new ComponentInstanceModel<ScriptModel>(new ScriptModel());
     private data: ScriptModel;
+    private fileProvider: DevExpress.fileManagement.ObjectFileSystemProvider;
 
     getData = () => {
         let builtObject = this.componentInstanceModel.getBuiltObject();
         return {
             ...this.data,
-            ...builtObject
+            ...builtObject,
+            scriptDirectoryContent: (this.fileProvider as any)._data
         } as ScriptModel;
     };
 
@@ -47,10 +51,15 @@ export class ScriptOptionsUI implements IOptionUI {
                 </div>
             `)
         }
-    ]
+    ];
 
     constructor(data: TDataSource, readonly?: boolean) {
         this.data = data as ScriptModel;
+        this.fileProvider = new DevExpress.fileManagement.ObjectFileSystemProvider({
+            //data: this.data.scriptDirectoryContent
+            data: TESTE_AAA
+
+        });
 
         this.componentInstanceModel.addInstance(new InstanceProps({
             componentName: "dxTabPanel",
@@ -72,15 +81,35 @@ export class ScriptOptionsUI implements IOptionUI {
             }).dxTabPanel("instance")
         }))
 
-        const fileProvider = new DevExpress.fileManagement.ObjectFileSystemProvider({
-            data: TESTE_AAA
-        });
 
+        const onToolbarItemClick = (args: any) => {
+            if (args?.itemData?.options?.text == "Editar") {
+                let instance = this.componentInstanceModel.getInstanceProps("scriptFileManager").getInstance() as DevExpress.ui.dxFileManager;
+                let selectedItem = instance.getSelectedItems();
+                new ScriptPopUp();
+                let CNT_REQUIRE_INSTANCE = (window as any).CNT_REQUIRE_INSTANCE;
+                CNT_REQUIRE_INSTANCE.config({
+                    paths: {
+                        'vs': 'https://talesxavier1.github.io/GH-CDN/TypeScriptEditor/lib/monaco-editor/min/vs'
+                    }
+                });
+                CNT_REQUIRE_INSTANCE(['vs/editor/editor.main'], function () {
+                    let comp = document.getElementById('file-manager');
+                    if (!comp) { return }
+                    let editor = monaco.editor.create(comp, {
+                        value: atob(selectedItem[0].dataItem.content),
+                        language: 'typescript',
+                        theme: "vs-dark",
+                    });
+                });
+            }
+
+        }
         this.componentInstanceModel.addInstance(new InstanceProps({
             componentName: "dxFileManager",
             tagName: "scriptFileManager",
             instance: $('#scriptFileManager').dxFileManager({
-                fileSystemProvider: fileProvider,
+                fileSystemProvider: this.fileProvider,
                 itemView: {
                     mode: 'thumbnails',
                 },
@@ -97,14 +126,33 @@ export class ScriptOptionsUI implements IOptionUI {
                 customizeThumbnail(fileSystemItem) {
                     return ""
                 },
+                selectionMode: "single",
+                toolbar: {
+                    fileSelectionItems: [
+                        "download", "separator",
+                        "move", "separator",
+                        "copy", "separator",
+                        "rename", "separator",
+                        "delete", "separator",
+                        {
+                            widget: 'dxButton',
+                            options: {
+                                text: "Editar",
+                            },
+                            location: 'before',
 
+                        }
+                    ]
+
+                },
+                onToolbarItemClick,
             }).dxFileManager("instance"),
         }))
 
         const a = () => {
             let instance = this.componentInstanceModel.getInstanceProps("scriptFileManager");
             let aaa: any = instance.getInstanceValue()
-            let t = (fileProvider as any)._data;
+            let t = (this.fileProvider as any)._data;
             console.clear()
 
             const tempTextArea = document.createElement('textarea');
@@ -152,9 +200,12 @@ class ScriptPopUp {
                 showCloseButton: true,
             }).dxPopup("instance")
         }));
-        let a: any;
+
 
 
 
     }
 }
+
+
+
--- src/models/ScriptModel.ts
+++ src/models/ScriptModel.ts
@@ -1,10 +1,26 @@
-//
 import { ShapeModel } from "./ShapeModel"
 
+interface IScriptDirectoryFile extends IScriptDirectoryHeader {
+    size?: number,
+    dateModified?: string,
+    content?: string
+}
+
+interface IScriptDirectoryHeader {
+    name: string,
+    isDirectory: boolean,
+    __KEY__: string,
+    items?: Array<IScriptDirectoryFile>,
+}
+
+
 export interface IScriptModel {
+    scriptDirectoryContent: Array<IScriptDirectoryFile>
 }
 
 export class ScriptModel extends ShapeModel implements IScriptModel {
+    scriptDirectoryContent: Array<IScriptDirectoryFile> = [];
+
     constructor(ID?: string) {
         super("script", ID);
     }
